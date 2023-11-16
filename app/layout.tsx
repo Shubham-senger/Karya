548a93cf8be082a96240c3fe91b59cb0197fb638
capeofhope <98865989+capeofhope@users.noreply.github.com>
2023-11-16 14:47:40 +0530

    Add files via upload
    Making  first commit after adding search options, appearances changes using convex and clerk.

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import {Toaster} from "sonner";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css';
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { ModalProvider } from "@/components/providers/modal-provider";
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Karya',
+  description: 'The connected workspace where better faster work happens.',
+  icons:{
+    icon:[
+      {
+        media:"(prefers-color-scheme:light)",
+        url:"/favicon-light.png",
+        href:"/favicon-light.png",
+      },
+      {
+        media:"(prefers-color-scheme:dark)",
+        url:"/favicon-dark.png",
+        href:"/favicon-dark.png"
+      }
+    ]
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+          storageKey='karya-theme-2'
+          >
+            <Toaster position="bottom-center"/>
+            <ModalProvider/>
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  )
+}
